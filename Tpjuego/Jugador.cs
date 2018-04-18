7678ecbc8a70b097c3182e9bd69b93a59ed2b184
DESKTOP-0GJ0QNR\Santiago Porchietto <santiporchietto@hotmail.com>
2018-04-18 19:40:18 -0300

    Commit inicial

---
+++ Tpjuego/Jugador.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tpjuego
+{
+    public class Jugador
+    {
+        public string IdConnection { get; set; }
+    }
+}
---
+++ Tpjuego/Partida.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tpjuego
+{
+    public class Partida
+    {
+        private static List<Partida> partidas = new List<Partida>();
+
+        private int Codigo { get; set; }
+
+        private Partida()
+        {
+
+        }
+
+        public int RetornarCodigo()
+        {
+            return this.Codigo;
+        }
+
+        public Partida CrearVerificarPartida(int codigo)
+        {
+            var partida = partidas.SingleOrDefault(x => x.Codigo == codigo);
+
+            if (partida == null)
+            {
+                partida = new Partida();
+                partidas.Add(partida);
+            }
+
+            return partida;
+        }
+
+    }
+}
